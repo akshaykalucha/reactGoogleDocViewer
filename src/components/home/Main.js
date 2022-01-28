79f2935d886a51bd380854ebab789f643308eac4
akshaykalucha3 <akshaykalucha@gmail.com>
2022-01-28 18:31:37 +0530

    new

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -5,7 +5,6 @@ export default class Main extends Component {
     render() {
         return (
             <div>
-                <p>this is test change</p>
                 <h1>Hello World</h1>
                 <embed src={mypdf} type="application/pdf" width="100%" height="600px" />
             </div>
