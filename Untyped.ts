a8daad87999f28c3e1e3d745fccf6052a4a10b90
akshaykalucha3 <akshaykalucha@gmail.com>
2022-03-15 14:03:17 +0530

    changes

---
+++ Untyped.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+ const path = require('path');
+ const fs = require('fs-extra');
+ 
+ /**
+  * Copy all untyped and static assets files to lib.
+  */
