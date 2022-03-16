bde246acb4ee3bbb6892f21f3583f46255f76304
akshaykalucha3 <akshaykalucha@gmail.com>
2022-03-16 14:06:48 +0530

    changes in render file

--- renderScree.js
+++ renderScree.js
@@ -2,22 +2,22 @@ import * as React from 'react'
 import { renderToString } from 'react-dom/server'
 import { AppRegistry } from 'react-native'
 
-function replaceRenderer({
-  bodyComponent,
-  replaceBodyHTMLString,
-  setHeadComponents,
-}) {
-    const RootComponent = () => bodyComponent
+// function replaceRenderer({
+//   bodyComponent,
+//   replaceBodyHTMLString,
+//   setHeadComponents,
+// }) {
+//     const RootComponent = () => bodyComponent
   
-    AppRegistry.registerComponent('main', () => RootComponent)
-    const { element, getStyleElement } = AppRegistry.getApplication('main')
+//     AppRegistry.registerComponent('main', () => RootComponent)
+//     const { element, getStyleElement } = AppRegistry.getApplication('main')
   
-    const markup = renderToString(element)
-    const styleElement = getStyleElement()
+//     const markup = renderToString(element)
+//     const styleElement = getStyleElement()
   
-    replaceBodyHTMLString(markup)
-    setHeadComponents([styleElement])
-  }
+//     replaceBodyHTMLString(markup)
+//     setHeadComponents([styleElement])
+//   }
   
   exports.replaceRenderer = replaceRenderer
 
