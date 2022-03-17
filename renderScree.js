7eac6e443596b65aa1971129f146f8209ef16409
akshaykalucha3 <akshaykalucha@gmail.com>
2022-03-17 14:07:36 +0530

    changes in render file

--- renderScree.js
+++ renderScree.js
@@ -6,7 +6,20 @@ function replaceRenderer({
     bodyComponent,
     replaceBodyHTMLString,
     setHeadComponents,
-  })
+  }) {
+    const RootComponent = () => bodyComponent
+  
+    AppRegistry.registerComponent('main', () => RootComponent)
+    const { element, getStyleElement } = AppRegistry.getApplication('main')
+  
+    const markup = renderToString(element)
+    const styleElement = getStyleElement()
+  
+    replaceBodyHTMLString(markup)
+    setHeadComponents([styleElement])
+  }
+  
+  exports.replaceRenderer = replaceRenderer
   
   exports.replaceRenderer = replaceRenderer
 
